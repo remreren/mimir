c85cdb14f25db2041f1513176b567be915e0a90e
Emre Eren <eeren881@gmail.com>
2024-10-22 16:52:57 +0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ExampleChart from '@/components/charts/ExampleChart.vue'
+</script>
+
+<template>
+  <main>
+    <ExampleChart />
+  </main>
+</template>
+
+<style scoped>
+.main {
+  width: 100%;
+}
+</style>
